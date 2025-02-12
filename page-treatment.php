d4d4db169bb4bab886c703afb444da0a787c25b3
nanasse_777 <nanase.webdesigner@gmail.com>
2025-02-12 14:11:51 +0900

    [add]「治療を知る」ページを追加

---
+++ page-treatment.php
@@ -0,0 +1,57 @@
+<?php
+get_header();
+?>
+
+<?php
+// メインビューをテンプレート化。
+// 表示内容はtenplate-parts/mainview.phpにて設定
+get_template_part('template-parts/mainview');
+?>
+
+<div class="u-bg-orange-gray">
+  <?php
+  // パンくずリスト
+  get_template_part('template-parts/breadcrumb');
+  ?>
+  <div class="l-base">
+    <ul class="c-small-box-list">
+      <li>
+        <h2 class="c-h3 c-small-box-list__h1">PTSDの治療</h2>
+        <div class="c-small-box-list__txt-box">
+          <p>PTSDには治療法があります。治療によって、症状を治したり、症状の強さが軽くなったりする可能性があります。そして、症状によって滞っていた日常生活や人間関係が、スムーズに行くようになる可能性があります。これまでの研究から、トラウマに焦点を当てた心理療法と薬物療法の両方が、PTSDの治療に有効であることが証明されています。</p>
+        </div>
+        <a href="./treatment-ptsd" class="c-btn">PTSDの治療を知る　<i class="fas fa-angle-right"></i></a>
+      </li>
+      <li>
+        <h2 class="c-h3 c-small-box-list__h1">CPTとは</h2>
+        <div class="c-small-box-list__txt-box">
+          <p>認知処理療法（CPT）は、心的外傷後ストレス症（PTSD）や関連症状に対する認知行動療法（思考や感情に焦点を当てた治療）です。PTSDの症状やその他の問題からの回復を滞らせる可能性のある「考え」の傾向に気づき、対処する術を身につけます。</p>
+        </div>
+        <a href="./about-cpt" class="c-btn">CPTを知る　<i class="fas fa-angle-right"></i></a>
+      </li>
+      <li>
+        <h2 class="c-h3 c-small-box-list__h1">CPTの進め方</h2>
+        <div class="c-small-box-list__txt-box">
+          <p>CPTは、全12回の各セッションの中で、どんなことに取り組むかが決まっている療法です。支援を求める人にとって、今、CPTに取り組むことが役立つかを判断したうえで、決まった手順に則り実施することで、PTSDや関連する症状の改善に効果を発揮します。</p>
+        </div>
+        <a href="./steps" class="c-btn">CPTの進め方を知る　<i class="fas fa-angle-right"></i></a>
+      </li>
+      <li>
+        <h2 class="c-h3 c-small-box-list__h1">CPTに親しむ</h2>
+        <div class="c-small-box-list__txt-box">
+          <p>当サイトをご覧になって、CPTを活用したい・より詳しく知りたいと思われた方は、下記の資料もご覧ください。自分自身でCPTを理解するほか、PTSDやトラウマにお悩みの方への支援の一助としてもご活用いただけると幸いです。</p>
+        </div>
+        <a href="./familiarize" class="c-btn">CPTに親しむを知る　<i class="fas fa-angle-right"></i></a>
+      </li>
+    </ul>
+
+  </div>
+
+</div>
+
+<?php
+// 「サイトについて」の子画面共通パーツ
+get_template_part('template-parts/about-common-area');
+?>
+
+<?php get_footer(); ?>
--- template-parts/mainview.php
+++ template-parts/mainview.php
@@ -60,6 +60,12 @@ if (is_page('about')) {
   $img_url_pc .= 'learn/learn-topview-pc.jpg';
   $img_url_sp .= 'learn/learn-topview-sp.jpg';
   $txt = 'トラウマを経験した後、長い期間反応が続く場合は、特別なケアが必要かもしれません。<br>症状が1か月以上続き、生活に支障をきたしている場合は、心的外傷後ストレス症（PTSD）を患っている可能性があります。';
+} else if (is_page('treatment')) {
+  $ttl = '治療を知る';
+  $img_url_pc .= 'treatment/treatment-topview-pc.jpg';
+  $img_url_sp .= 'treatment/treatment-topview-sp.jpg';
+  $txt = 'けがをした時と同じように、心が傷ついた時にも、手当てが必要です。トラウマの経験によって傷ついたとしても、多くの人は数週間で回復に向かっていきます。もし、自然には癒えない痛みを抱えているなら、専門的な治療が役に立つかもしれません。 ';
+  $is_parent_page = true;
 }
 
 ?>
@@ -70,6 +76,9 @@ if (is_page('about')) {
   } ?>
   <?php if (is_page('learn') || is_parent_slug() == 'learn') {
     echo ' -learn';
+  } ?>
+  <?php if (is_page('treatment') || is_parent_slug() == 'treatment') {
+    echo ' -treatment';
   } ?>
   ">
   <?php if ($tag): ?>
