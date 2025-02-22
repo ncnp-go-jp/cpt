f10f588d3480c99ba7a3f58ba94402b0602f230c
akiramori <xxxakiramorixxx@gmail.com>
2025-02-22 22:03:48 +0900

    [update]タグマネージャーの設置
    [update]タグマネージャーの設置

--- header.php
+++ header.php
@@ -2,6 +2,24 @@
 <html lang="ja">
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-KS2SDSFL');
+  </script>
+  <!-- End Google Tag Manager -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,6 +30,10 @@
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KS2SDSFL"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <header class="p-hdr">
     <div class="l-wide p-hdr__inner">
       <h1><a href="<?php echo home_url(); ?>">CPT</a></h1>
